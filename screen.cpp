a69ba667928c1107c9136e28b3b9014bbfd791d0
Victor LI <victor-li@outlook.com>
2017-11-23 19:28:45 +0800

    Implement screen class

--- screen.cpp
+++ screen.cpp
@@ -4,3 +4,47 @@ screen::screen(QWidget *parent) : QWidget(parent)
 {
 
 }
+screen::screen(QLabel *l)
+{
+    m_label = l;
+    m_frameRate = 30;
+    m_timer = new QTimer();
+    m_timer->setInterval(1000/m_frameRate);
+    m_timer->start();
+    connect(m_timer,SIGNAL(timeout()),this,SLOT(refreshImage()));
+}
+
+void screen::newImage(Mat matImg)
+{
+    if(m_frameMutex.tryLock())
+    {
+        m_frameQue.enqueue(matImg);
+        m_frameMutex.unlock();
+    }
+    return;
+}
+
+void screen::refreshImage()
+{
+    if(m_frameMutex.tryLock())
+    {
+        if(!m_frameQue.empty())
+        {
+            Mat frame = m_frameQue.dequeue();
+            m_frameSize.setWidth(frame.cols);
+            m_frameSize.setHeight(frame.rows);
+            m_pixmap = m_pixmap.fromImage(Mat2QImage(frame));
+            m_label->setPixmap(m_pixmap);
+        }
+        m_frameMutex.unlock();
+    }
+}
+
+QImage screen::Mat2QImage(cv::Mat const& src)
+{
+     cv::Mat temp; // make the same cv::Mat
+     cvtColor(src, temp,CV_BGR2RGB); // cvtColor Makes a copt, that what i need
+     QImage dest((const uchar *) temp.data, temp.cols, temp.rows, temp.step, QImage::Format_RGB888);
+     dest.bits(); // enforce deep copy, see documentation
+     return dest;
+}
--- screen.h
+++ screen.h
@@ -2,25 +2,38 @@
 #define SCREEN_H
 
 #include <QWidget>
+#include <QLabel>
+#include <QPixmap>
+#include <QQueue>
+#include <QMutex>
+#include <QTimer>
 #include <opencv2/opencv.hpp>
 using  namespace cv;
+using namespace std;
 
 class screen : public QWidget
 {
     Q_OBJECT
 public:
     explicit screen(QWidget *parent = nullptr);
-    void screen(QWidget *);
+    explicit screen(QLabel * l);
+    static QImage screen::Mat2QImage(cv::Mat const& src);
 
 signals:
 
 public slots:
-    void UpdateImage(Mat matImg);
+    void newImage(Mat matImg);
+    void refreshImage();
 
 private:
-    int m_frameRate;
+    int m_frameRate =0;
+    QSize m_frameSize;
     QRect m_targetLoc;
-
+    QLabel *m_label;
+    QPixmap m_pixmap;
+    QQueue<Mat> m_frameQue;
+    QMutex m_frameMutex;
+    QTimer *m_timer;
 };
 
 #endif // SCREEN_H
