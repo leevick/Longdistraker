a03933176d16745f6a1431b6f96766a609195da5
Victor LI <victor-li@outlook.com>
2017-11-25 19:24:43 +0800

    Implement guide camera class

--- guidecamera.cpp
+++ guidecamera.cpp
@@ -1,6 +1,54 @@
 #include "guidecamera.h"
 
+
 guideCamera::guideCamera()
 {
+    PylonInitialize();
+}
 
+guideCamera::~guideCamera()
+{
+    close();
+    PylonTerminate();
+}
+
+bool guideCamera::open(int id)
+{
+    m_camera = new CInstantCamera( CTlFactory::GetInstance().CreateFirstDevice());
+    m_camera->MaxNumBuffer = 5;
+    m_camera->StartGrabbing();
+    return true;
+}
+
+void guideCamera::close()
+{
+
+}
+
+bool guideCamera::isOpen()
+{
+    return m_camera->IsGrabbing();
+}
+
+bool guideCamera::getNextFrame(cv::Mat *grab)
+{
+    if (!grab)
+        return false;
+
+    m_camera->RetrieveResult(5000,m_ptrGrabResult);
+    if(m_ptrGrabResult->GrabSucceeded())
+    {
+        imageSize = getImageSize();
+        grab->create(imageSize.height(),imageSize.width(),CV_8U);
+        grab->data = (uchar *)m_ptrGrabResult->GetBuffer();
+    }
+    return !grab->empty();
+}
+
+// Always fixed using OpenCV interface to USB guideCameras
+QSize guideCamera::getImageSize()
+{
+    if(m_ptrGrabResult->GrabSucceeded())
+        return QSize(m_ptrGrabResult->GetWidth(),m_ptrGrabResult->GetHeight());
+    else return QSize(0,0);
 }
--- guidecamera.h
+++ guidecamera.h
@@ -1,11 +1,26 @@
 #ifndef GUIDECAMERA_H
 #define GUIDECAMERA_H
+#include <camera.h>
+#include <pylon/PylonIncludes.h>
 
+using namespace Pylon;
+using namespace std;
 
 class guideCamera : public Camera
 {
 public:
     guideCamera();
+    ~guideCamera();
+    bool open(int id);
+    void close();
+    bool isOpen();
+    bool getNextFrame(cv::Mat *grab);
+    QSize getImageSize();
+    QSize imageSize;
+private:
+    CInstantCamera *m_camera;
+    CGrabResultPtr m_ptrGrabResult;
+
 };
 
 #endif // GUIDECAMERA_H
