5908ebf863a1eda1798139dfe4aca9af2738ba47
Victor LI <victor-li@outlook.com>
2017-12-15 00:37:15 +0800

    Add log as a public method; Add connectCamera slot; Modified Camera pointer; Add isConnected as a private attribute; Modified workflow;

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -5,13 +5,35 @@ MainWindow::MainWindow(QWidget *parent) :
     QMainWindow(parent),
     ui(new Ui::MainWindow)
 {
-    ui->setupUi(this);
-    m_screen = new screen(ui->labelImage);
-    m_camera = new guideCamera();
-    m_camera->open(0);
-    m_threadcapture = new threadCapture();
-    connect(m_threadcapture,SIGNAL(newImage(cv::Mat)),m_screen,SLOT(newImage(cv::Mat)),Qt::DirectConnection);
-    m_threadcapture->startCapture(m_camera);
+    try
+    {
+        //Initilize UI
+        ui->setupUi(this);
+        ui->comboBoxCameraSelection->addItem("Web Camera");
+        ui->comboBoxCameraSelection->addItem("Guidance Camera");
+        ui->comboBoxCameraSelection->addItem("Imaging Camera");
+        ui->comboBoxCameraSelection->addItem("Simulation Source");
+        
+        //Allocate Memory
+        m_screen = new screen(ui->labelImage);
+        m_camera = new Camera *[4];
+        m_camera[0] = new WebCam();
+        m_camera[1] = new guideCamera();
+        m_camera[2] = new imagingCamera();
+
+        //Connect signals and slots
+        connect(ui->pushButtonOpenOrClose,SIGNAL(clicked(bool)),this,SLOT(connectCamera()));
+        
+        // m_camera->open(0);
+        // m_threadcapture = new threadCapture();
+        // connect(m_threadcapture,SIGNAL(newImage(cv::Mat)),m_screen,SLOT(newImage(cv::Mat)),Qt::DirectConnection);
+        // m_threadcapture->startCapture(m_camera);
+        log("Main window created");
+    }
+    catch(QException e)
+    {
+        log("Main window creation failed!");
+    }
 
 }
 
@@ -21,8 +43,81 @@ MainWindow::~MainWindow()
     m_threadcapture->terminate();
     disconnect(this,0,0,0);
     delete m_threadcapture;
-    m_camera->close();
-    delete m_camera;
+    for(int i=0;i<4;i++)
+    {
+        m_camera[i]->close();
+        delete m_camera[i];
+    }
     delete m_screen;
     delete ui;
+}
+
+void MainWindow::log(QString s)
+{
+    QDateTime time;
+    time = time.currentDateTime();
+    ui->log->insertPlainText(time.toString("HH:MM:ss")+"\t"+s+"\n");
+    ui->log->moveCursor(QTextCursor::End);
+    return;
+}
+
+void MainWindow::connectCamera()
+{
+    if(!isConnected)
+    {
+        try
+        {
+            log("Connecting to "+ui->comboBoxCameraSelection->itemText(ui->comboBoxCameraSelection->currentIndex())+"...");
+            ui->pushButtonOpenOrClose->setDisabled(true);
+            ui->comboBoxCameraSelection->setDisabled(true);
+            m_camera[ui->comboBoxCameraSelection->currentIndex()]->open(0);
+            m_threadcapture = new threadCapture();
+            connect(m_threadcapture,SIGNAL(newImage(cv::Mat,int)),m_screen,SLOT(newImage(cv::Mat,int)),Qt::DirectConnection);
+            m_threadcapture->startCapture(m_camera[ui->comboBoxCameraSelection->currentIndex()]);
+
+            isConnected = true;
+            ui->pushButtonOpenOrClose->setText("Disconnect");
+            ui->pushButtonOpenOrClose->setDisabled(false);
+            log("Connection to "+ui->comboBoxCameraSelection->itemText(ui->comboBoxCameraSelection->currentIndex())+" established!");
+        }
+        catch(QException e)
+        {
+            log("Attempt to connect "+ui->comboBoxCameraSelection->itemText(ui->comboBoxCameraSelection->currentIndex())+" failed!");
+            m_threadcapture->terminate();
+            disconnect(m_threadcapture);
+            delete m_threadcapture;
+            ui->pushButtonOpenOrClose->setDisabled(false);
+            ui->comboBoxCameraSelection->setDisabled(false);
+        }
+    }else
+    {
+        try
+        {
+            log("Disconnecting from "+ui->comboBoxCameraSelection->itemText(ui->comboBoxCameraSelection->currentIndex())+"...");
+            ui->pushButtonOpenOrClose->setDisabled(true);
+
+            m_threadcapture->stopCapture();
+            m_threadcapture->terminate();
+            disconnect(m_threadcapture);
+            delete m_threadcapture;
+            m_camera[ui->comboBoxCameraSelection->currentIndex()]->close();
+
+            isConnected = false;
+            ui->pushButtonOpenOrClose->setText("Connect");
+            ui->pushButtonOpenOrClose->setDisabled(false);
+            ui->comboBoxCameraSelection->setDisabled(false);
+
+            log("Disconnected from "+ui->comboBoxCameraSelection->itemText(ui->comboBoxCameraSelection->currentIndex())+"!");
+
+        }
+        catch(QException e)
+        {
+            log("Attempt to disconnect from "+ui->comboBoxCameraSelection->itemText(ui->comboBoxCameraSelection->currentIndex())+" failed!");
+            m_threadcapture->terminate();
+            disconnect(m_threadcapture);
+            delete m_threadcapture;
+        }
+    }
+
+    return;
 }
--- mainwindow.h
+++ mainwindow.h
@@ -3,10 +3,12 @@
 
 #include <QMainWindow>
 #include <QException>
+#include <QDateTime>
 #include <screen.h>
 #include <threadcapture.h>
 #include <webcamera.h>
 #include <guidecamera.h>
+#include <imagingcamera.h>
 
 namespace Ui {
 class MainWindow;
@@ -19,12 +21,18 @@ class MainWindow : public QMainWindow
 public:
     explicit MainWindow(QWidget *parent = 0);
     ~MainWindow();
+    void log(QString s);
+
+public slots:
+    void connectCamera();
 
 private:
     Ui::MainWindow *ui;
     screen *m_screen;
     threadCapture *m_threadcapture;
-    Camera * m_camera;
+    Camera **m_camera;
+
+    bool isConnected = false;
 };
 
 #endif // MAINWINDOW_H
