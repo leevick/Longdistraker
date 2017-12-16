8c455a83c447a660804f95d587e8236dd7093799
Victor <victor-li@outlook.com>
2017-12-16 22:44:25 +0800

    Partially implement imaging camera.

--- imagingcamera.cpp
+++ imagingcamera.cpp
@@ -5,8 +5,141 @@ imagingCamera::imagingCamera()
 
 }
 imagingCamera::~imagingCamera(){}
-bool imagingCamera::open(int id){return true;}
+bool imagingCamera::open(int id)
+{
+    try
+    {
+        QList<QString> boardNames;
+
+        // start up
+        iPortCountCheck = getBoardInfo(boardNames);
+        if((iRet = clGetNumSerialPorts(&iPortCount)) == CL_ERR_NO_ERR && iPortCount == iPortCountCheck ){
+
+             emit selectSerialPort(boardNames,&iPortNr);
+           // initialize serial port
+          if((iRet = clSerialInit(iPortNr, &hSer)) != CL_ERR_NO_ERR){
+            // get size of Buffer
+            clGetErrorText(iRet, NULL, &iErrTextSize);
+            if( iErrTextSize > 0 ){
+              char *ErrText = NULL;
+              // retrieve the Buffer
+              ErrText = new char[iErrTextSize];
+              clGetErrorText(iRet, ErrText, &iErrTextSize);
+              throw Exception("Cannot open connection to CameraLink port:"+QString(ErrText));
+              delete ErrText;
+            }
+            return false;
+          }
+
+          // set baudrate
+          if ( (iRet = clSetBaudRate(hSer, clSerBaudRate)) != CL_ERR_NO_ERR) {
+            // get size of Buffer
+            clGetErrorText(iRet, NULL, &iErrTextSize);
+            if( iErrTextSize > 0 ){
+              char *ErrText = NULL;
+              ErrText = new char[iErrTextSize];
+              clGetErrorText(iRet, ErrText, &iErrTextSize);
+              delete ErrText;
+            }
+            throw Exception("Cannot set baud rate to requested value");
+            return false;
+          }
+          
+          
+
+        }
+        else{
+
+          return false;
+        }
+
+        return true;
+    }
+    catch(Exception e)
+    {
+        throw e;
+        return false;
+    }
+    return true;
+}
 void imagingCamera::close(){return;}
 bool imagingCamera::isOpen(){return true;}
 bool imagingCamera::getNextFrame(cv::Mat *grab){return true;}
 QSize imagingCamera::getImageSize(){return QSize(0,0);}
+
+int imagingCamera::getBoardInfo(QList<QString> &list)
+{
+  int boardType;
+  int i = 0;
+
+  int maxNrOfboards = 10;// use a constant no. of boards to query, when evaluations versions minor to RT 5.2
+  int nrOfBoardsFound = 0;
+  int maxBoardIndex = -1;
+  int iPortCount = 0;
+  int nrOfBoardsPresent = 0;
+
+  list.clear();
+
+  // detect all boards
+  nrOfBoardsPresent = getNrOfBoards();
+  if(!nrOfBoardsPresent)
+    return 0;
+
+  for(i = 0; i < maxNrOfboards; i++) {
+    int iPortNrOnBoard = 0;
+    const char * boardName;
+    bool skipIndex = false;
+    boardType = Fg_getBoardType(i);
+    switch(boardType) {
+      case PN_MICROENABLE4AS1CL:
+        boardName = "MicroEnable IV AS1-CL";
+        iPortNrOnBoard = 1;
+        break;
+      case PN_MICROENABLE4AD1CL:
+        boardName = "MicroEnable IV AD1-CL";
+        iPortNrOnBoard = 1;
+        break;
+      case PN_MICROENABLE4VD1CL:
+        boardName = "MicroEnable IV VD1-CL";
+        iPortNrOnBoard = 2;
+        break;
+      case PN_MICROENABLE4AD4CL:
+        boardName = "MicroEnable IV AD4-CL";
+        iPortNrOnBoard = 2;
+        break;
+      case PN_MICROENABLE4VD4CL:
+        boardName = "MicroEnable IV VD4-CL";
+        iPortNrOnBoard = 2;
+        break;
+      // ignore the non-cameralink boards
+      case PN_MICROENABLE3I:
+      case PN_MICROENABLE3IXXL:
+      case PN_MICROENABLE4AQ4GE:
+      case PN_MICROENABLE4VQ4GE:
+      default:
+        boardName = "Unknown / Unsupported Board";
+        skipIndex = true;
+    }
+    if (!skipIndex){
+      nrOfBoardsFound++;
+      maxBoardIndex = i;
+
+      if(iPortNrOnBoard > 0){
+        //if(i==0)
+        //printf("Following serial ports are available:\n");
+        for(int j = 0; j < iPortNrOnBoard; j++){
+          iPortCount++;
+
+          //printf("%d. Board_%u %s (%x) Port_%d\n", iPortCount-1, i, boardName, boardType, j);
+          list.append(QString::number(iPortCount-1,10)+"."+"Board_"+QString::number(i,10)+" "+QString(boardName)+" ("+QString::number(boardType,16)+") Port_"+QString::number(j,10));
+        }
+      }
+    }
+    else{
+    }
+    if (nrOfBoardsFound >= nrOfBoardsPresent){
+      break;// all boards are scanned
+    }
+  }
+  return iPortCount;
+}
--- imagingcamera.h
+++ imagingcamera.h
@@ -1,10 +1,24 @@
 #ifndef IMAGINGCAMERA_H
 #define IMAGINGCAMERA_H
 
+#include <Exception>
+#include <QMessageBox>
+#include <QList>
+
 #include <camera.h>
+
+#include <clser.h>
+#include <board_and_dll_chooser.h>
+#include <os_type.h>
+
+#include <fgrab_struct.h>
+#include <fgrab_prototyp.h>
+#include <fgrab_define.h>
+#include <SisoDisplay.h>
 
 class imagingCamera : public Camera
 {
+    Q_OBJECT
 public:
     imagingCamera();
     ~imagingCamera();
@@ -13,6 +27,20 @@ public:
     bool isOpen();
     bool getNextFrame(cv::Mat *grab);
     QSize getImageSize();
+signals:
+    void selectSerialPort(const QList<QString> &boardNames,int *selectedPort);
+
+private:
+    void *hSer = NULL;  // reference to serial port
+    int iPortNr = -1;
+    unsigned int iPortCount = 0;  // count of serial port in whole system
+    int iPortCountCheck = -1;
+    int iRet = CL_ERR_NO_ERR;
+
+    unsigned int iErrTextSize = 0;
+    unsigned int clSerBaudRate = CL_BAUDRATE_9600;	// baud rate of serial port, normally 9600
+
+    int imagingCamera::getBoardInfo(QList<QString> &list);
 };
 
 #endif // IMAGINGCAMERA_H
