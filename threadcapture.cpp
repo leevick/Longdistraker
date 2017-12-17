4739378a6645efb228669f757424473f0bd950c7
Victor <victor-li@outlook.com>
2017-12-17 23:21:28 +0800

    Add fixed frame rate mode.

--- threadcapture.cpp
+++ threadcapture.cpp
@@ -1,7 +1,20 @@
 #include "threadcapture.h"
 
-bool threadCapture::startCapture(Camera *camera)
+bool threadCapture::startCapture(Camera *camera,int interval)
 {
+    desiredInterval = interval;
+    countTimer = new QTimer();
+    captureTimer = new QTimer();
+
+    if(interval!=0)
+    {
+        desiredFrameRate = 1000/interval;
+    }
+    else
+    {
+        desiredFrameRate = 0;
+    }
+
     if (isRunning())
         return false;
 
@@ -14,8 +27,18 @@ bool threadCapture::startCapture(Camera *camera)
     if (!m_camera->startCapture())
         return false;
 
-    start(QThread::TimeCriticalPriority);
 
+    countTimer->setInterval(1000);
+    connect(captureTimer,SIGNAL(timeout()),this,SLOT(captureTimeoutHandler()));
+    connect(countTimer,SIGNAL(timeout()),this,SLOT(countTimeoutHandler()));
+    countTimer->start();
+
+    if(desiredInterval!=0)
+    {
+        captureTimer->setInterval(desiredInterval);
+        captureTimer->start();
+    }
+    start(QThread::HighestPriority);
     return true;
 }
 
@@ -26,16 +49,45 @@ void threadCapture::stopCapture()
 
 void threadCapture::run()
 {
-    cv::Mat grab;
-    cv::Mat detect;
-
-    while (!m_stop) {
-        if (!m_camera->getNextFrame(&grab)) {
-            msleep(10);
-            continue;
-        }
-        emit newImage(grab,m_camera->colorConversion);
+    if(desiredInterval!=0)
+    {
+        while(!m_stop)
+        {
+            if (!m_camera->getNextFrame(&grab)) 
+            {
+                continue;
+            }
+        };
     }
-
+    else
+    {
+        while(!m_stop)
+        {
+            if (!m_camera->getNextFrame(&grab)) 
+            {
+                continue;
+            }
+            emit newImage(grab,m_camera->colorConversion);
+            frameCount++;
+        };
+        
+    }
     m_camera->stopCapture();
+}
+
+void threadCapture::captureTimeoutHandler()
+{
+    frameCount++;
+    //videoWriter->write(*grab);
+    emit newImage(grab,m_camera->colorConversion);
+
+}
+
+void threadCapture::countTimeoutHandler()
+{
+    std::cout<<"current actual frame rate="<<frameCount<<std::endl;
+    actualFrameRate = frameCount;
+    emit log("current actual frame rate="+QString::number(frameCount,10));
+    frameCount = 0;
+
 }
--- threadcapture.h
+++ threadcapture.h
@@ -2,6 +2,7 @@
 #define THREADCAPTURE_H
 
 #include <QThread>
+#include <QTimer>
 #include <QObject>
 #include "camera.h"
 
@@ -10,18 +11,31 @@ class threadCapture : public QThread
 {
     Q_OBJECT
     public:
-        bool startCapture(Camera *camera);
+        bool startCapture(Camera *camera,int interval=0);
         void stopCapture();
 
     signals:
         void newImage(cv::Mat grab,int colorConversion);
+        void log(QString s);
 
     protected:
         void run();
 
     private:
         Camera *m_camera;
+        int desiredInterval;
+        int desiredFrameRate = 0;
+        int actualFrameRate = 0;
+        cv::VideoWriter *videoWriter;
+        cv::Mat         grab;
+        QTimer *captureTimer;
+        QTimer *countTimer;
     volatile bool m_stop;
+    volatile int frameCount=0;
+
+    private slots:
+    void captureTimeoutHandler();
+    void countTimeoutHandler();
 };
 
 #endif // THREADCAPTURE_H
