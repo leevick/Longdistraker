99c6ffbd3b85e16b1be4ed25b0ec86c92cb3679f
Victor <victor-li@outlook.com>
2017-12-20 18:06:59 +0800

    Reconstruct camera interface.

--- camera.h
+++ camera.h
@@ -2,27 +2,58 @@
 #define CAMERA_H
 
 #include <QThread>
+#include <QQueue>
 #include <QSize>
+#include <QTimer>
+#include <QMetaType>
 #include <exception.h>
-
 #include <opencv2/opencv.hpp>
 
-class Camera : protected QThread
+using namespace std;
+class Camera : public QObject
 {
+    Q_OBJECT
 public:
-    int colorConversion;
-    Camera() {}
-    virtual ~Camera() {}
+    Camera() {qRegisterMetaType<QQueue<cv::Mat>>("QQueue<cv::Mat>");}
+    virtual ~Camera(){}
+    virtual void open() = 0;
 
-    virtual bool open(int device) = 0;
+    virtual int getFramesPerGrab(){return 1;};
+    virtual void setFramePerGrab(){};
+
+    virtual void setFps(){}
+    virtual double getFps(){return fps;}
+
+    virtual void setExposureTime(){}
+    virtual double getExposureTime(){return 0;}
+
+    virtual int getColorConversion(){return colorConversion;}
+
+    virtual int  getHeight(){return height;}
+    virtual int  getWidth(){return width;}
     virtual void close() = 0;
-    virtual bool isOpen() = 0;
-    virtual bool startCapture() { return true; }
-    virtual bool stopCapture() { return true; }
-    virtual bool getNextFrame(cv::Mat *grab) = 0;
+    virtual bool isOpen(){return m_isOpen;}
+    virtual bool isCapturing(){return m_isCapturing;}
 
-    // should be available after open()
-    virtual QSize getImageSize() = 0;
+    public slots:
+    virtual void handleStartRequest()=0;
+    virtual void handleStopRequest()=0;
+    
+
+protected:
+
+    bool m_isCapturing=false;
+    bool m_isOpen = false;
+    int width=0;
+    int height=0;
+    int fps=0;
+    int framesPerGrab = 1;
+    double exposureTime=0;
+    int colorConversion=0;
+    QTimer *captureTimer;
+    cv::Mat *imageBuffer;
+protected slots:
+    virtual void handleTimeout()=0;
 };
 
 #endif // CAMERA_H
