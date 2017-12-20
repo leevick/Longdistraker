2618a9db986f68352c746a1bbe99c9d26cf33b83
Victor <victor-li@outlook.com>
2017-12-20 18:09:19 +0800

    Implement recorder class.

--- recorder.cpp
+++ recorder.cpp
@@ -4,3 +4,46 @@ Recorder::Recorder()
 {
 
 }
+
+void Recorder::handleStartRequest(imagingCamera *cam)
+{
+    try
+    {
+        //emit selectVideoPath(videoPath);
+        video = new cv::VideoWriter();
+        isRecording = true;
+        cv::Mat temp;
+        temp.create(768,1024,CV_8U);
+        video->open("test.avi",cv::VideoWriter::fourcc('M','J','P','G'),20.0,cv::Size(1024,768),false);
+        lastid = Fg_getLastPicNumberEx(cam->fg,0,cam->pMem0);
+
+        while(isRecording)
+        {
+            long long currentid = Fg_getLastPicNumberEx(cam->fg,0,cam->pMem0);
+            if(lastid<currentid)
+            {
+                temp.data = (uchar *)Fg_getImagePtrEx(cam->fg,lastid,0,cam->pMem0);
+                video->write(temp);
+                lastid++;
+            }else if(lastid==currentid)
+            {
+                continue;
+            }
+            else
+            {
+                lastid = currentid;
+            }
+        }
+        video->release();
+    }
+    catch(Exception e)
+    {
+        throw e;
+    }
+}
+
+void Recorder::handleStopRequest()
+{
+    isRecording = false;
+    return;
+}
--- recorder.h
+++ recorder.h
@@ -1,11 +1,35 @@
 #ifndef RECORDER_H
 #define RECORDER_H
+#include<QQueue>
+#include<QThread>
+//#include<threadcapture.h>
+#include<Exception.h>
+#include<opencv2/opencv.hpp>
+#include<imagingcamera.h>
 
-
-class Recorder : public QThread
+using namespace std;
+class Recorder : public QObject
 {
+    Q_OBJECT
 public:
     Recorder();
+
+signals:
+    void selectVideoPath(string &path);
+
+public slots:
+    void handleStartRequest(imagingCamera *cam);
+    void handleStopRequest();
+private:
+    volatile bool isRecording = false;
+    string videoPath;
+    cv::VideoWriter *video;
+    unsigned char *pBuffer;
+    Fg_Struct *fg;
+    long long lastid=0;
+
+    
+
 };
 
 #endif // RECORDER_H
